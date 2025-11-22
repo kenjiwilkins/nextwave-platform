205978e9a34ccbf964da11e8f673b7c382fb0902
Michael Kenji Wilkins <kenji.wilkins@gmail.com>
2025-11-22 21:47:04 +0900

    add: empty state

--- packages/web/src/app/dashboard/page.tsx
+++ packages/web/src/app/dashboard/page.tsx
@@ -1,3 +1,11 @@
+import { EmployeeEmptyState } from "@/features/employee/components";
+
 export default async function ProjectDashboardPage() {
-  return <div>Project Dashboard Page</div>;
+  return (
+  <main className="flex h-screen bg-background">
+    <div className="flex-1 flex flex-col">
+      <EmployeeEmptyState />
+    </div>
+  </main>
+);
 }
